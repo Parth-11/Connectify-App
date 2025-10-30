442bfd01061218db0b32dbfacc74b5bc700c1370
Parth-11 <parth12.mgg@gmail.com>
2025-10-30 14:45:23 +0530

    Setup navigation to different tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Connectify());
+  runApp(Connectify());
 }
 
-class Connectify extends StatelessWidget {
+class Connectify extends StatefulWidget {
   const Connectify({super.key});
+
+  @override
+  State<Connectify> createState() => _ConnectifyState();
+}
+
+class _ConnectifyState extends State<Connectify> {
+  int curIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +24,14 @@ class Connectify extends StatelessWidget {
           title: Text('Connectify'),
           leading: IconButton(onPressed: () {}, icon: Icon(Icons.abc)),
         ),
+        body: Container(),
         bottomNavigationBar: BottomNavigationBar(
+          currentIndex: curIndex,
+          onTap: (value) {
+            setState(() {
+              curIndex = value;
+            });
+          },
           items: [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
             BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),

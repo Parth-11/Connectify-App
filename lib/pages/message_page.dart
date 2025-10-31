ae21924cdf816bc0c89911ac0891eb6b645258d0
Parth-11 <parth12.mgg@gmail.com>
2025-10-31 21:02:11 +0530

    added padding to page

--- lib/pages/message_page.dart
+++ lib/pages/message_page.dart
@@ -11,80 +11,96 @@ class _MessagePageState extends State<MessagePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                'Chatting',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
-              ),
-              Spacer(),
-              GestureDetector(child: Icon(Icons.search, size: 32)),
-            ],
-          ),
-          Row(
-            children: [
-              Text(
-                'Story',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
-              ),
-              Spacer(),
-              GestureDetector(child: Icon(Icons.more_horiz, size: 24)),
-            ],
-          ),
-          SizedBox(
-            height: 100,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
               children: [
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    height: 64,
-                    width: 64,
-                    decoration: ShapeDecoration(
-                      color: Colors.grey.shade200,
-                      shape: CircleBorder(
-                        side: BorderSide(
-                          color: Colors.grey.shade400,
-                          width: 2,
-                          style: BorderStyle.solid,
+                Text(
+                  'Chatting',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                ),
+                Spacer(),
+                GestureDetector(child: Icon(Icons.search, size: 32)),
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  'Story',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+                ),
+                Spacer(),
+                GestureDetector(child: Icon(Icons.more_horiz, size: 24)),
+              ],
+            ),
+            SizedBox(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 84,
+                      width: 84,
+                      decoration: ShapeDecoration(
+                        color: Colors.grey.shade300,
+                        shape: CircleBorder(
+                          side: BorderSide(
+                            color: Colors.grey.shade500,
+                            width: 2,
+                            style: BorderStyle.solid,
+                          ),
                         ),
                       ),
+                      child: Icon(Icons.add),
                     ),
-                    child: Icon(Icons.add),
                   ),
-                ),
-                CircleAvatar(radius: 32),
-              ],
+                  CircleAvatar(radius: 42),
+                ],
+              ),
             ),
-          ),
-          Row(
-            children: [
-              Text('Chat'),
-              Spacer(),
-              GestureDetector(child: Icon(Icons.more_horiz)),
-            ],
-          ),
-          SizedBox(
-            height: 100,
-            child: ListView(
+            Row(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    CircleAvatar(),
-                    Column(
-                      children: [Text("Sophia Williams"), Text("Lorem Ipsum")],
-                    ),
-                    Text('Time'),
-                  ],
+                Text(
+                  'Chat',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
                 ),
+                Spacer(),
+                GestureDetector(child: Icon(Icons.more_horiz, size: 24)),
               ],
             ),
-          ),
-        ],
+            SizedBox(
+              height: 100,
+              child: ListView(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CircleAvatar(radius: 32),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Sophia Williams",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Text("Lorem Ipsum"),
+                        ],
+                      ),
+                      Text('Time'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

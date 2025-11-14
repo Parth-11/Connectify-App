673c1243e7bb3ebb98a4b2857157a0e8c2adfa20
AdityaTaggar05 <aditya05.mgg@gmail.com>
2025-11-15 03:42:16 +0530

    [ui] Added UI for Sign In page

--- lib/pages/signin/widgets/signup_text.dart
+++ lib/pages/signin/widgets/signup_text.dart
@@ -1,9 +1,9 @@
-import 'package:connectify/pages/signin/signin.dart';
+import 'package:connectify/pages/signup/signup.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class SignInText extends StatelessWidget {
-  const SignInText({super.key});
+class SignUpText extends StatelessWidget {
+  const SignUpText({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,16 +12,16 @@ class SignInText extends StatelessWidget {
       text: TextSpan(
         style: TextStyle(color: Colors.grey.shade500),
         children: [
-          const TextSpan(text: "Already have an account? "),
+          const TextSpan(text: "No Account? "),
           TextSpan(
-            text: "Sign In",
+            text: "Sign Up",
             style: const TextStyle(
               color: Colors.blueAccent,
               fontWeight: FontWeight.w600,
             ),
             recognizer: TapGestureRecognizer()
               ..onTap = () => Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (_) => SignInPage()),
+                MaterialPageRoute(builder: (_) => SignUpPage()),
               ),
           ),
         ],

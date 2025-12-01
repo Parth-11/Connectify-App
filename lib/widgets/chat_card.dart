3c720edec40c7ccbdc2f18231ead867d86c0698a
Parth-11 <parth12.mgg@gmail.com>
2025-12-01 22:43:28 +0530

    added chat page template

--- lib/widgets/chat_card.dart
+++ lib/widgets/chat_card.dart
@@ -24,19 +24,26 @@ class ChatCard extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              CircleAvatar(radius: 32),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Sophia Williams",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                  ),
-                  Text("Lorem Ipsum"),
-                ],
+              CircleAvatar(radius: 38),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Sophia Williams",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text("Lorem Ipsum", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
               ),
+              Spacer(),
               Text('Time'),
             ],
           ),

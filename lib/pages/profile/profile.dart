3e59376ed8b7e05f52dee76d92a36e9d9ec4702c
AdityaTaggar05 <aditya05.mgg@gmail.com>
2025-12-11 17:18:31 +0530

    [ui] Changed profile page ui and added animation

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -3,8 +3,59 @@ import 'package:flutter/material.dart';
 import '/pages/settings/settings.dart';
 import '/widgets/rounded_icon_button.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage>
+    with SingleTickerProviderStateMixin {
+  final headerKey = GlobalKey();
+  late AnimationController _controller;
+  late Animation<double> animation;
+
+  double columnHeight = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _measureColumnHeight();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _measureColumnHeight() {
+    final renderBox =
+        headerKey.currentContext?.findRenderObject() as RenderBox?;
+    if (renderBox != null) {
+      setState(() {
+        columnHeight = renderBox.size.height;
+
+        animation = Tween<double>(begin: 0, end: columnHeight).animate(
+          CurvedAnimation(parent: _controller, curve: Curves.easeOutBack),
+        );
+
+        Future.delayed(
+          Duration(milliseconds: 300),
+          () => _controller.forward(),
+        );
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,68 +63,79 @@ class ProfilePage extends StatelessWidget {
 
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  RoundedIconButton(
-                    icon: Icons.arrow_back_rounded,
-                    onTap: Navigator.of(context).pop,
-                    color: theme.colorScheme.surface,
-                  ),
-                  const Spacer(),
-                  RoundedIconButton(
-                    icon: Icons.settings_rounded,
-                    onTap: () => Navigator.of(
-                      context,
-                    ).push(MaterialPageRoute(builder: (_) => SettingsPage())),
-                    color: theme.colorScheme.surface,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 32),
-              Row(
-                children: [
-                  Hero(
-                    tag: "pfp",
-                    child: Container(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                if (columnHeight > 0)
+                  AnimatedBuilder(
+                    animation: _controller,
+                    builder: (_, _) => Container(
+                      height: animation.value,
                       decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: theme.colorScheme.primaryContainer,
-                      ),
-                      height: 60,
-                      width: 60,
-                      child: Icon(
-                        Icons.account_circle,
-                        color: theme.colorScheme.onPrimaryContainer,
+                        color: theme.colorScheme.tertiary,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(32),
+                          bottomRight: Radius.circular(32),
+                        ),
                       ),
                     ),
                   ),
-                  SizedBox(width: 16),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "John Doe",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
+                Column(
+                  key: headerKey,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 16, 16, 32),
+                      child: Row(
+                        children: [
+                          RoundedIconButton(
+                            icon: Icons.arrow_back_rounded,
+                            onTap: Navigator.of(context).pop,
+                            color: theme.colorScheme.surface,
+                          ),
+                          const Spacer(),
+                          RoundedIconButton(
+                            icon: Icons.settings_rounded,
+                            onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(builder: (_) => SettingsPage()),
+                            ),
+                            color: theme.colorScheme.surface,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Hero(
+                      tag: "pfp",
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: theme.colorScheme.onTertiary,
+                        ),
+                        height: 80,
+                        width: 80,
+                        child: Icon(
+                          Icons.account_circle,
+                          color: theme.colorScheme.onPrimaryContainer,
+                          size: 36,
                         ),
                       ),
-                      Text(
-                        "Active",
-                        style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      "John Doe",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: theme.colorScheme.onTertiary,
                       ),
-                    ],
-                  ),
-                ],
-              ),
-              Divider(height: 48),
-            ],
-          ),
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
